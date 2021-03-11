93362aaed8100cc589285e6e17700260db1bcee9
mitra47 <73024958+mitra47@users.noreply.github.com>
2021-03-11 10:41:31 +0300

    11.03.2021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -44,7 +44,7 @@ namespace WebAPI
             services.AddControllers();
             //services.AddSingleton<IProductService,ProductManager>();
             //services.AddSingleton<IProductDal, EfProductDal>();
-           
+            services.AddCors();
             var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
 
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
@@ -73,6 +73,8 @@ namespace WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
